d6a004afd530170272a4479f5677b837c8e8140a
Jiying <jiyingzou@gmail.com>
2020-05-04 21:26:02 -0700

    started data integrity check script

---
+++ Data_Cleaning_Scripts/china_NHC_dataintegritychecks.R
@@ -0,0 +1,9 @@
+data = read.csv("~/Documents/School/Grad/Q3/CS472/COVID19_projections/Data/chinadata_nhc.csv")
+#data = data[-nrow(data),]
+write.csv(data, file = "~/Documents/School/Grad/Q3/CS472/COVID19_projections/Data/chinadata_nhc.csv", row.names = F)
+
+#Variables
+names(data)
+
+#Check if new cases sum up to total
+sum((cumsum(data$confirmed_new) + 214) != data$confirmed_ttl)

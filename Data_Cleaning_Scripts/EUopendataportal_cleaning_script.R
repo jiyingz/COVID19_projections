8fbf034d8bc12c778ae0d20340757f0f2b9d302c
Jiying <jiyingzou@gmail.com>
2020-05-20 13:26:29 -0700

    added data source link

--- Data_Cleaning_Scripts/EUopendataportal_cleaning_script.R
+++ Data_Cleaning_Scripts/EUopendataportal_cleaning_script.R
@@ -1,3 +1,4 @@
+#Data source: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data
 df = read.csv("~/Downloads/data_europa_covid19cases", sep = ",")
 
 df = df %>% 
@@ -11,3 +12,6 @@ df = df %>%
 df$countriesAndTerritories = gsub("_", " ", df$countriesAndTerritories)
 df$countriesAndTerritories[which(df$countriesAndTerritories == "United Kingdom")] = "UK"
 df$countriesAndTerritories[which(df$countriesAndTerritories == "United States of America")] = "US"
+
+write.csv(df, file = "~/Documents/School/Grad/Q3/CS472/COVID19_projections/Data/world_cases_deaths_EUopendataportal.csv")
+

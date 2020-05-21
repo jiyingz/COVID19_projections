45fb6e45316d1ca900281fdd4255473d255b0e95
Kofi Adu <kadu25@stanford.edu>
2020-05-20 19:02:24 -0700

    Add script to get country data and add Brazil and India data

---
+++ Data/get_country_data.py
@@ -0,0 +1,30 @@
+import sys, getopt
+import pandas as pd
+
+
+def output_country_data(country):
+   world_data = pd.read_csv("world_cases_deaths_recovered_JHU.csv")
+   country_data = world_data.loc[world_data['country'] == country]
+   if country_data.empty:
+      print("Error:", country, "does not exist in the dataset")
+   else:
+      country_data.to_csv("./" + str(country) + "_data.csv")
+
+def main(argv):
+   country = ''
+   try:
+      opts, args = getopt.getopt(argv,"h:c:",["country="])
+   except getopt.GetoptError:
+      print('get_country_data.py -c <country>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('get_country_data.py -c <country>')
+         sys.exit()
+      elif opt in ("-c", "--country"):
+         country = arg
+   output_country_data(country)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
